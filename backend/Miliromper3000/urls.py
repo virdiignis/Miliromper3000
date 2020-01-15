ca79a324d648620c8af6890db0c75c5a8bf0fcfb
virdiignis <gbryk1@gmail.com>
2020-01-15 21:12:12 +0100

    serializers and urls

--- backend/Miliromper3000/urls.py
+++ backend/Miliromper3000/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path,re_path
+from rest_framework import routers
+from dbAlco import views
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('^', include(router.urls))
 ]
---
+++ backend/dbAlco/serializers/alcoserializers.py
@@ -0,0 +1,4 @@
+from rest_framework.serializers import *
+from dbAlco.models.alcohol import *
+
+
