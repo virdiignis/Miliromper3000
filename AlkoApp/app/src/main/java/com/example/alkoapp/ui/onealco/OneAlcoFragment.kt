6f2f27c6c7845d9b3d1d7b9513a36924e8b5d372
makram89 <34419130+makram89@users.noreply.github.com>
2020-01-26 14:30:44 +0100

    +

--- AlkoApp/app/src/main/java/com/example/alkoapp/ui/onealco/OneAlcoFragment.kt
+++ AlkoApp/app/src/main/java/com/example/alkoapp/ui/onealco/OneAlcoFragment.kt
@@ -1,16 +1,16 @@
 package com.example.alkoapp.ui.onealco
 
-import androidx.lifecycle.ViewModelProviders
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.RecyclerView
-import com.example.alkoapp.R
-import com.example.alkoapp.data.models.Alcohol
+    import androidx.lifecycle.ViewModelProviders
+    import android.os.Bundle
+    import androidx.fragment.app.Fragment
+    import android.view.LayoutInflater
+    import android.view.View
+    import android.view.ViewGroup
+    import androidx.appcompat.app.AppCompatActivity
+    import androidx.databinding.DataBindingUtil
+    import androidx.recyclerview.widget.RecyclerView
+    import com.example.alkoapp.R
+    import com.example.alkoapp.data.models.Alcohol
 import com.example.alkoapp.databinding.OneAlcoFragmentBinding
 
 
@@ -28,19 +28,16 @@ class OneAlcoFragment(val itemAlcohol: Alcohol) : Fragment() {
         binding.alcohol = itemAlcohol
         return binding.root
 
-//        return inflater.inflate(R.layout.one_alco_fragment, container, false)
     }
 
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(OneAlcoViewModel::class.java)
-        
+
     }
 
 
 }
 
-class AlcoholHolder(
-    val oneAlcoFragmentBinding: OneAlcoFragmentBinding
-)
+
