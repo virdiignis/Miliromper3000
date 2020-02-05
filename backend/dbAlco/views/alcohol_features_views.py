1ecb654419d38ec9126ca83e92bf75786966431b
virdiignis <gbryk1@gmail.com>
2020-02-05 18:48:52 +0100

    add filtering and ordering according to this link: https://www.django-rest-framework.org/api-guide/filtering/#generic-filtering

--- backend/dbAlco/views/alcohol_features_views.py
+++ backend/dbAlco/views/alcohol_features_views.py
@@ -1,15 +1,17 @@
 from dbAlco.serializers.alcohol_features_serializers import *
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, filters
 
 
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    filter_backends = [filters.OrderingFilter]
 
 
 class ProducerViewSet(viewsets.ModelViewSet):
     queryset = Producer.objects.all()
     serializer_class = ProducerSerializer
+    filter_backends = [filters.OrderingFilter]
 
 
 class AlcoholRatingViewSet(viewsets.ModelViewSet):
