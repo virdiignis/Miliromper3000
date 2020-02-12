8bf712453c16e5fc9a96f6c4069dc02ff4b14488
virdiignis <gbryk1@gmail.com>
2020-02-12 06:25:55 +0100

    register layout

--- AlkoApp/app/src/main/java/com/example/alkoapp/ui/home/RegisterFragment.kt
+++ AlkoApp/app/src/main/java/com/example/alkoapp/ui/home/RegisterFragment.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.alkoapp.R
 
-class HomeFragment : Fragment() {
-    private lateinit var homeViewModel: HomeViewModel
+class RegisterFragment : Fragment() {
+    private lateinit var homeViewModel: RegisterViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,12 +17,8 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+            ViewModelProviders.of(this).get(RegisterViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_register, container, false)
         return root
     }
 }
---
+++ AlkoApp/app/src/main/java/com/example/alkoapp/ui/home/RegisterViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.alkoapp.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+}
